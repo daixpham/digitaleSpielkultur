79ee089ddc378fd8e77623518edf7af97d7f13f3
Dai Pham <xuan.pham@st.ovgu.de>
2019-11-20 23:32:29 +0100

    layout added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000
 const path = require('path');
+app.use(express.static(__dirname + '/style/'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
